220687625b13aa2a612238c7bc35c01d1ac39b4c
Anastasiya Kislach <anastasiya.kislach@gmail.com>
2016-11-15 22:44:52 +0300

    js for ckeditor (choose file)

---
+++ SalonLesanj.Presenter.UI/cke_getIdInputUrl.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function () {
+    $('.returnImage').click('click',
+        function (e) {
+            var urlImage = $(this).attr('data-url');
+
+            var doc = window.opener.document;
+            var id = doc.getElementsByTagName('label')[3].control.id;
+            doc.getElementById(id).value = urlImage;
+
+            window.close();
+        });
+});
+
+//<style type="text/css">
+//   .returnImage { 
+//    margin: 10px;
+//    padding: 5px;
+//    width: 210px;
+//   }
+  
+//.returnImage img{
+//    box-sizing: border-box;
+//    border: 1px solid black;
+//    padding: 5px;
+//}
+//</style>
