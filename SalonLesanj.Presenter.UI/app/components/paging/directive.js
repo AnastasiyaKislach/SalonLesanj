070604cab7b2a53eb271a025acfa708d55978c96
AnastasiyaKislach <anastasiya.kislach@gmail.com>
2016-12-20 12:45:23 +0300

    release version

--- SalonLesanj.Presenter.UI/app/components/paging/directive.js
+++ SalonLesanj.Presenter.UI/app/components/paging/directive.js
@@ -10,15 +10,15 @@
         templateUrl: 'app/components/paging/view.html',
         replace: true,
         link: function (scope, element, attr) {
-            var pages = scope.pages = [];
+            var pages = scope.globalPages = [];
 
             function initialize() {
                 var pageSize = ($routeParams.pageSize && parseInt($routeParams.pageSize)) || scope.defaultPageSize || 6;
                 var pageIndex = (($routeParams.pageIndex && parseInt($routeParams.pageIndex)) || 1) - 1;
                 var totalPages = Math.ceil(scope.items.length / pageSize);
-                scope.pages = [];
+                scope.globalPages = [];
                 for (var i = 0; i < totalPages; i++) {
-                    scope.pages.push({
+                    scope.globalPages.push({
                         index: i + 1,
                         path: $location.path() + '?pageIndex=' + (i + 1) + '&pageSize=' + pageSize,
                         isFirst: i == 0,
@@ -27,7 +27,16 @@
                         hasLast: i > 0
                     });
                 }
-                scope.activePage = scope.pages[pageIndex];
+
+                var before = Math.floor(scope.maxSize / 2);
+                var after = scope.maxSize - before - 1;
+
+                var start = (pageIndex - before) === -1 ? 0 : (pageIndex - before);
+                var end = (pageIndex + after + 1) < scope.maxSize ? scope.maxSize : (pageIndex + after + 1);
+
+                scope.activePage = scope.globalPages[pageIndex];
+
+                scope.pages = scope.globalPages.slice(start, end);
             }
             initialize();
         }
